7b7f89391cb2a901e900e8400896526d68438595
tet <gmorer@student.42.fr>
2016-06-04 12:31:08 +0200

    colours

--- src/prompt.c
+++ src/prompt.c
@@ -4,11 +4,16 @@ void static	allenv(char *temp, char *temp1, char *temp2)
 {
 	if (ft_strstr(temp, temp1) != NULL)
 	{
+		ft_putstr("\x1b[31m");
 		ft_putstr(temp2);
+		ft_putstr("\x1b[32m");
 		ft_putstr(temp + ft_strlen(temp1));
 	}
 	else
+	{
+		ft_putstr("\x1b[32m");
 		ft_putstr(temp);
+	}
 }
 
 void		prompt(char **env, int returnvalue)
@@ -16,9 +21,11 @@ void		prompt(char **env, int returnvalue)
 	char	*temp1;
 	char	*temp2;
 	char	*temp;
-	ft_putchar('[');
+	
+	ft_putstr("\x1b[34m[\e[1m");
 	ft_putnbr(returnvalue);
-	ft_putstr("] -> ");
+	ft_putstr("]");
+	ft_putstr("\x1b[33m -> ");
 	temp = getcwd(NULL, 0);
 	temp1 = getenvline(env, "HOME=");
 	temp2 = getenvline(env, "USER=");
@@ -29,7 +36,12 @@ void		prompt(char **env, int returnvalue)
 		free(temp1);
 	}
 	else
+	{
+		ft_putstr("\x1b[32m");
 		ft_putstr(temp);
+	}
 	free(temp);
+	ft_putstr("\x1b[36m");
 	ft_putstr(": ");
+	ft_putstr("\x1b[0m");
 }
